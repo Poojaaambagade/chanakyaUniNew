dec2ef9c2ebd7a6ddc90741417ff835c8bb7456d
poojaaambagade <poojaambagade@gmail.com>
2023-07-26 11:54:20 +0530

    customvalidation

--- MyAngularProject/src/app/sign-up/sign-up.component.ts
+++ MyAngularProject/src/app/sign-up/sign-up.component.ts
@@ -7,22 +7,32 @@ import { Router } from '@angular/router';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
+
+
 export class SignUpComponent {
+  
   signUpForm!: FormGroup;
+
+
   constructor(private router: Router,
     private formBuilder: FormBuilder) { }
+
+
 
   ngOnInit() {
     console.log('-----');
     this.signUpFormControl();    //signupformcontrol function ngoninit ke andr q likha bcz jb compo load hoga tab form b load hona chaiye
   }
+
+
   signUpFormControl() {
     this.signUpForm = this.formBuilder.group({
       name: ['', [Validators.required,Validators.pattern('[a-zA-Z ]*$'), Validators.minLength(10)]],
       mobNo: ['',[Validators.pattern('[0-9]*$'),Validators.minLength(10),Validators.maxLength(10)]],
       city: ['',[Validators.pattern('[A-Za-z]*$'),Validators.minLength(3)]],
       address: ['',[Validators.pattern('[A-Za-z0-9@ -]*$')]],
-      gender: ['male']
+      // gender: ['male'], (its getting by default selected radio button)
+      gender:['',[Validators.required]] //(both can be select)
     })
 
   }
--- MyAngularProject/src/app/student/student-activity/student-activity.component.ts
+++ MyAngularProject/src/app/student/student-activity/student-activity.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-activity',
@@ -7,4 +8,58 @@ import { Component } from '@angular/core';
 })
 export class StudentActivityComponent {
 
+  signInForm!: FormGroup;
+  showSignInForm: boolean = false;
+  passwordValue:any;
+  confirmPasswordValue:any;
+  isMatch!:boolean;
+  constructor(private fb: FormBuilder) { }
+
+
+  showForm() {
+
+    //this.showSignInForm = true;
+    this.formDef();
+    this.showSignInForm = !this.showSignInForm;
+
+  }
+  formDef() {
+
+    this.signInForm = this.fb.group({
+      password: [],
+      confpassword: [],
+      name:['',this.nameValidation]
+    })
+  }
+  //custom function
+  nameValidation(inp:any){
+    console.log(inp.value);
+    let data=inp.value?.toLowerCase(); //(? value hogi to tolowercase me convert hoga otherwise error dega spo ? lagaya hai optional hota hai ? se)
+    let isErr=data.includes('copy');
+    return isErr ? {Err:true} : null;
+    
+
+  }
+
+  password() {
+    // console.log('------', this.signInForm.value.password);
+    // console.log("--");
+    this.passwordValue=this.signInForm.value.password;
+    if(this.passwordValue == this.confirmPasswordValue){
+      this.isMatch=true;
+    }
+    else{
+      this.isMatch=false;
+    }
+  }
+  confirmPassword(){
+  this.confirmPasswordValue=this.signInForm.value.confpassword;
+  if(this.passwordValue==this.confirmPasswordValue){
+    this.isMatch=true;
+  }
+  else{
+    this.isMatch=false;
+  }
+  } 
+  
 }
